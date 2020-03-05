2af4ed8fc4f3f2b64a17a75bc802d5e679c91afb
jamesespericueta <espericueta2303@gmail.com>
2020-03-04 23:46:19 -0500

    started the pytorch version, need to fix the back prop because loss doesn't seem to vary

--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,145 @@
-import matplotlib.pyplot as plt
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
 import torch
-import torchvision
-from torchvision import transforms, datasets
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import torch.optim as optim
 
-train = datasets.MNIST('', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ]))
 
-test = datasets.MNIST('', train=False, download=True,
-                      transform=transforms.Compose([
-                          transforms.ToTensor()
-                      ]))
+class LEFTVSRIGHT():
+    LEFT = "data/train/left"
+    RIGHT = "data/train/right"
+    TESTING = "data/validation"
+    LABELS = {LEFT: 0, RIGHT: 1}
+    training_data = []
 
-trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
-testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=False)
+    left_count = 0
+    right_count = 0
 
-for data in trainset:
-    print(data)
-    break
+    def make_training_data(self):
+        for label in self.LABELS:
+            print(label)
+            for f in tqdm(os.listdir(label)):
+                if "jpg" in f:
+                    try:
+                        path = os.path.join(label, f)
+                        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (50, 50))
+                        self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])
+                        if label == self.LEFT:
+                            self.left_count += 1
+                        elif label == self.RIGHT:
+                            self.right_count += 1
 
-X, y = data[0][0], data[1][0]
+                    except Exception as e:
+                        print(path, str(e))
 
-plt.imshow(data[0][0].view(28, 28))
-plt.show()
+        np.random.shuffle(self.training_data)
+        np.save("training_data.npy", self.training_data)
+        print('Left:', self.left_count)
+        print('Right:', self.right_count)
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50, 50).view(-1, 1, 50, 50)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 2)
+
+    def convs(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+
+net = Net()
+print(net)
+
+REBUILD_DATA = False
+
+if REBUILD_DATA:
+    leftvsright = LEFTVSRIGHT()
+    leftvsright.make_training_data()
+
+training_data = np.load('training_data.npy', allow_pickle=True)
+
+loss_function = nn.MSELoss()
+
+X = torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50)
+X = X/255.0
+y = torch.Tensor([i[1] for i in training_data])
+
+VAL_PCT = 0.1
+val_size = int(len(X)*VAL_PCT)
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+
+test_X = X[:-val_size]
+test_y = y[:-val_size]
+
+optimizer = optim.Adam(net.parameters())
+
+
+def train(net):
+    BATCH_SIZE = 300
+    EPOCHS = 40
+    for epoch in range(EPOCHS):
+        for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
+            batch_X = train_X[i:i+BATCH_SIZE].view((-1, 1, 50, 50))
+            batch_y = train_y[i:i+BATCH_SIZE]
+
+            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+
+            net.zero_grad()
+            outputs = net(batch_X)
+
+            matches = [torch.argmax(i)==torch.argmax(j) for i, j in zip(outputs, batch_y)]
+            in_sample_acc = matches.count(True)/len(matches)
+
+            loss = loss_function(outputs, batch_y)
+            loss.backward()
+            optimizer.step()
+        print(loss)
+        print("In sample acc:", round(in_sample_acc, 2))
+
+def test(net):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i in tqdm(range(len(test_X))):
+            real_class = torch.argmax(test_y[i]).to(device)
+            net_out = net(test_X[i].view(-1, 1, 50, 50))[0].to(device)
+            predicted_class = torch.argmax(net_out)
+
+            if predicted_class == real_class:
+                correct += 1
+            total += 1
+
+    print("Accuracy: ", round(correct/total, 3))
+
+
+device = torch.device("cuda:0")
+print("Running on the GPU")
+net = Net().to(device)
+train(net)
